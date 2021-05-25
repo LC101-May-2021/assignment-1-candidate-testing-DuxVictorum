eb020e163e4a0b363b3372ae89627afcd1d8a908
DuxVictorum <>
2021-05-25 01:50:32 +0000

    Completed all parts for 1.1.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +16,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+console.log("\nOkay, here's our first question:");
+candidateAnswer = input.question("\t" + question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer === correctAnswer) {
+	console.log("That's right, it was Sally Ride in 1983.")
+} else {
+	console.log("Sorry, that is incorrect.")
+}
 
   let grade;
   
@@ -37,8 +42,9 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
+  // TODO 1.1c: Greet candidate by name //
+  console.log(`\nWelcome, ${candidateName}, let's get started.`)
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
