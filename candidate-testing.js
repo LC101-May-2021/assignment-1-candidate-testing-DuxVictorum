5a200d86a5840f3680fe645c96b4689abea3b626
DuxVictorum <>
2021-05-31 04:59:53 +0000

    Completed part 2 (all 5 questions)

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,43 +1,69 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
+let questions = [
+	"Who was the first American woman in space? ", 
+	"True or false: 5 kilometer == 5000 meters? ",
+	"(5 + 3)/2 * 10 = ? ",
+	"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+	"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+	"Sally Ride",
+	"true",
+	"40",
+	"Trajectory",
+	"3"
+];
+let candidateAnswers = [];
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+let question = "";
+let correctAnswer = "";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ");
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log("\nOkay, here's our first question:");
-candidateAnswer = input.question("\t" + question);
+  console.log("\nHere are the questions:" )
+  for (let i = 0; i < questions.length; i++) {
+	  question = questions[i];
+	  candidateAnswer = input.question("#" + String(i + 1) + ": " + question);
+	  candidateAnswer = candidateAnswer.toLowerCase();
+	  candidateAnswers.push(candidateAnswer);
+  }
+  console.log(candidateAnswers);
 }
 
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswers) {
+	console.log("Candidate name:", candidateName);
+	for (let i = 0; i < candidateAnswers.length; i++) {
+		console.log(`${String(i + 1)}): ${questions[i]}
+	Your answer: ${candidateAnswers[i]}
+	Correct answer: ${correctAnswers[i]}`);
+	
+		// let correctLower = correctAnswers[i].toLowerCase();
+		// if (candidateAnswers[i] === correctLower[i]) {
+		// 	console.log("Correct!");
+		// } else {
+		// 	console.log("Sorry, incorrect.");
+		// }
+	}
+	
+	
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-	console.log("That's right, it was Sally Ride in 1983.")
-} else {
-	console.log("Sorry, that is incorrect.")
-}
-
-  let grade;
+  	let grade;
   
 
-  return grade;
+  	return grade;
 }
 
 function runProgram() {
